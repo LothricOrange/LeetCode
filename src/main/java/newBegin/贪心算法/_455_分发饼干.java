00f3b2d4a6bf46d4028b0778f37fde2ec83b19b2
LothricOrange <yingjie.www@gmail.com>
2023-04-10 23:45:45 +0800

    "update"

---
+++ src/main/java/newBegin/贪心算法/_455_分发饼干.java
@@ -0,0 +1,19 @@
+package newBegin.贪心算法;
+
+import java.util.Arrays;
+
+public class _455_分发饼干 {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int start = 0;
+        int count = 0;
+        for (int i = 0; i < s.length && start < g.length; i++) {
+            if (s[i] >= g[start]) {
+                start++;
+                count++;
+            }
+        }
+        return count;
+    }
+}
