7d61a88509b7b7df4527f65586a0562250d4f188
LothricOrange <yingjie.www@gmail.com>
2023-05-02 23:45:51 +0800

    "update"

---
+++ src/main/java/newBegin/回溯算法/_51_N皇后.java
@@ -0,0 +1,53 @@
+package newBegin.回溯算法;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _51_N皇后 {
+    int[] cols;
+    List<List<String>> list = new ArrayList<>();
+    public List<List<String>> solveNQueens(int n) {
+        cols = new int[n];
+        MyQueen(0);
+        return list;
+    }
+
+    public void MyQueen(int row) {
+        if (row == cols.length) {
+            list.add(show());
+            return;
+        }
+
+        for (int col = 0; col < cols.length; col++) {
+            if (isLegal(row, col)) {
+                cols[row] = col;
+                MyQueen(row + 1);
+            }
+        }
+    }
+
+    public boolean isLegal(int row, int col) {
+        for (int i = 0; i < row; i++) {
+            if (cols[i] == col) return false;
+            if (row - i == Math.abs(cols[i] - col)) return false;
+        }
+        return true;
+    }
+
+    public List<String> show() {
+        List<String> stringList = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        for (int row = 0; row < cols.length; row++) {
+            for (int col = 0; col < cols.length; col++) {
+                if (cols[row] == col) {
+                    sb.append("Q");
+                } else {
+                    sb.append(".");
+                }
+            }
+            stringList.add(sb.toString());
+            sb = new StringBuilder();
+        }
+        return stringList;
+    }
+}
