6ae80ad191dc0b3d2bd7a79af353059ae594dfba
LothricOrange <yingjie.www@gmail.com>
2023-07-17 23:41:20 +0800

    "每日一题"

---
+++ src/main/C/question_of_the_day/_27_移除元素.c
@@ -0,0 +1,11 @@
+int removeElement(int* nums, int numsSize, int val){
+    int fast;
+    int slow = 0;
+    for (fast = 0; fast < numsSize; ++fast) {
+        if (nums[fast] != val) {
+            nums[slow] = nums[fast];
+            ++slow;
+        }
+    }
+    return slow;
+}
